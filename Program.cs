f829e08060c4b1da721cc0b0485e373e25461acd
Abdul Qayyum <khoi00091@gmail.com>
2018-08-11 16:41:42 +0800

    add comment explanation

--- Program.cs
+++ Program.cs
@@ -12,32 +12,37 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-
+            
+            //Create Object
             Vampire vampire = new Vampire();
             vampire.HitPoint = 15;
             Human human = new Human();
             human.HitPoint = 60;
             Undead undead = new Undead();
             undead.HitPoint = 80;
-
+            
+            //create list 
             List<Creature> creatures = new List<Creature>();
+            //add object into list
             creatures.Add(vampire);
             creatures.Add(human);
             creatures.Add(undead);
 
-
+            
+            //if hitpoint less then or equal to 15, able to used special ability
             UseSpecialAbilities(creatures, item => item.HitPoint <= 15);
 
             Console.ReadKey();
         }
 
 
-
+        //GET: list object, delegate object
         public static void UseSpecialAbilities(List<Creature> creatures,IsUsable isUsable)
         {
-
+            //loop every creature 
             foreach(var creature in creatures)
             {
+                
                 if (isUsable(creature))
                 {
                     creature.GetSpecialAbility();
